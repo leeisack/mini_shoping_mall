13fda75f0df363ded3e5a125dfb8a206d52ef803
isack Lee <isacklee@isackui-MacBookPro.local>
2020-12-25 16:44:54 +0900

    mini shoping mall

---
+++ main.js
@@ -0,0 +1,56 @@
+// Fetch the items from the Json file
+function loadItems(){
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+// Update the list with the given items
+function displayItems(items){
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item)).join('');
+    // console.log(html);
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+
+}
+// Create HTML list item from the given data item
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick(event, items){
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+    console.log(key);
+    console.log(value);
+
+    if(key == null || value == null){
+        return;
+    }
+    const filtered = items.filter(item=> item[key] === value);
+    console.log(filtered);
+    displayItems(items.filter(item => item[key] === value));
+}
+// SetEventListener
+function setEventListeners(items){
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', ()=> displayItems(items));
+    buttons.addEventListener('click', (event)=> onButtonClick(event, items))
+}
+
+
+
+//main
+loadItems()
+.then(items => {
+    console.log(items);
+    displayItems(items); 
+    setEventListeners(items)
+})
+.catch(console.log);
